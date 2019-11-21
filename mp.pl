1085a0b32a562ad83192013d2d15515349cfc05c
rdj519 <39096856+rdj519@users.noreply.github.com>
2019-11-21 14:17:22 +0800

    Add files via upload
    added the OR function if a certain predicate for symptom is already true, gives the 'no' option to retract the symptom (in the case of incorrect input or accidentally incorrect information).

--- mp.pl
+++ mp.pl
@@ -1,7 +1,7 @@
 :- dynamic has/2.
 :- dynamic person/1.
 
-person(person). /* can be deleted */
+
 has(person, symptom).
 
 asthma(X) :-
@@ -18,6 +18,9 @@ headPain(X) :-
     write(' You have headache.'), nl.
 
 start :- choose_category.
+
+retract_symptom(X, Y) :-
+    retract(has(X,Y)).
 
 choose_category:- write('Pick a category'), nl,
               write('1. Head'), nl,
@@ -38,28 +41,28 @@ choose_category:- write('Pick a category'), nl,
 head_category :- /* not yet done because we can retract symptoms */
 		write('Do you have headache?'), nl,
 		read(A),nl,
-		(A == yes -> assert(has(patient, headache)),nl ; nl),
+		(A == yes -> assert(has(patient, headache)),nl ; A == no -> has(patient, headache) -> retract(has(patient, headache));nl),
 		write('Are your eyes (or either of your eyes) irritated? '), nl,
 		read(B), nl,
-		(B == yes -> assert(has(patient, irritated_eyes)), nl; nl),
+		(B == yes -> assert(has(patient, irritated_eyes)), nl; B == no -> has(patient, irritated_eyes) -> retract(has(patient, irritated_eyes)); nl),
                 write('Do you have neck pain?'), nl,
                 read(C), nl,
-                (C == yes -> assert(has(patient, neck_pain)), nl; nl),
+                (C == yes -> assert(has(patient, neck_pain)), nl ;C == no -> has(patient, neck_pain) -> retract(has(patient, neck_pain)); nl),
                 write('Pale face?'), nl,
                 read(D), nl,
-                (D == yes -> assert(has(patient, pale_face)), nl; nl),
+                (D == yes -> assert(has(patient, pale_face)), nl; D == no -> has(patient, pale_face) -> retract(has(patient, pale_face));  nl),
                 write('Do you have a runny nose?'), nl,
                 read(E), nl,
-                (E == yes -> assert(has(patient, runny_nose)), nl; nl),
+                (E == yes -> assert(has(patient, runny_nose)), nl; E == no -> has(patient, runny_nose) -> retract(has(patient, runny_nose));   nl),
                 write('Are you having seizures?'), nl,
                 read(F), nl,
-                (F == yes -> assert(has(patient, seizures)), nl; nl),
+                (F == yes -> assert(has(patient, seizures)), nl; F == no -> has(patient, seizures) -> retract(has(patient, seizures));  nl),
                 write('Sensitive to light?'), nl,
                 read(G), nl,
-                (G == yes -> assert(has(patient, sensitivity_to_light)), nl; nl),
+                (G == yes -> assert(has(patient, sensitivity_to_light)), nl; G == no -> has(patient, sensitivity_to_light) -> retract(has(patient, sensitivity_to_light));  nl),
                 write('Do you have stiff neck? '), nl,
                 read(H), nl,
-                (H == yes -> assert(has(patient, stiff_neck)), nl; nl), nl,
+                (H == yes -> assert(has(patient, stiff_neck)), nl;  H == no -> has(patient, stiff_neck) -> retract(has(patient, stiff_neck)); nl), nl,
                 write('noted.'), nl,
                 choose_category.
 
